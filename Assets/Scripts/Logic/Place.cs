87c8dc1e70634fa1875d9647da58c574a320bb80
Tiniestguitar01 <126778880+Tiniestguitar01@users.noreply.github.com>
2024-09-13 05:41:24 +0200

    Logic scripts

---
+++ Assets/Scripts/Logic/Place.cs
@@ -0,0 +1,13 @@
+public class Place
+{
+    public int size;
+    public int[] placePosition;
+    public Direction direction;
+
+    public Place(int size, int[] placePosition, Direction direction)
+    {
+        this.size = size;
+        this.placePosition = placePosition;
+        this.direction = direction;
+    }
+}
